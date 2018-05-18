21b4e173932293319c6878c7028ac5cf78074e38
silviojaeger <silvio.jaeger@hotmail.com>
2018-05-18 11:22:32 +0200

    inverter and own moduls

--- start_solarmanager.js
+++ start_solarmanager.js
@@ -1,52 +1,41 @@
+//Import node modules
 var express = require('express');
 var app = express();
 var path = require('path');
 var request = require('request');
+var fs = require('fs');
+
+//Import own modules
+var inverter = require("./app/inverter");
+var routes = require("./app/routes")(app, inverter);
+
+//global variables
+var configJson;
+var inverterConfig;
+
+//-------TESTING-------------------------------------------------------------------------
+readConfig();
+
+inverter.setIp(inverterConfig.ip);
+inverter.setApi(inverterConfig.api);
+inverter.setIntervTime(inverterConfig.interval);
+
+//----------------------------------------------------------------------------------------
 
 
-var trbmbContent= null;
-
-//schickt public verzeichniss an client
+//send public folder to client
 app.use(express.static(path.join(__dirname, 'public')));
 
-//--------------------------------------------------------------------------------------------------------------------
-//Get Text everey 5 seconds
-var interval = setInterval(
-		function(){ 
-			requestSomething('http://api.chew.pro/trbmb') 
-		}, 5000
-);
 
-//--------------------------------------------------------------------------------------------------------------------
-
-//Routes werden hier definiert....können auch in separate JS ausgelagert werden---------------------------------------
-app.get('/', function (req, res) {
-  res.sendfile('./view/index.html');
-});
-
-app.get('/trbmb', function (req, res) {
-	res.setHeader('Content-Type', 'application/json');
-	res.send(trbmbContent);
-});
-
-
-//---------------------------------------------------------------------------------------------------------------------
+//reads the configuration File and initialize all devices
+function readConfig(){
+	configJson = JSON.parse(fs.readFileSync('./configs/config.json', 'utf8'));	//read config.json and store it to var configJson
+	inverterConfig= configJson.inverter;										//read Inverter	
+}
 
 //Server starten und Port definieren
 app.listen(3000, function () {
-  console.log('Solarmanager gestartet!');
+  console.log('Solarmanager server started!');
 });
-
-
-//Einfache HTTP Request Methode----------------------------------------------------------------------------------------
-function requestSomething(url){
-	request(url, function (error, response, body) {
-	  if(error!=null){console.log('error:', error);} 				// Print the error if one occurred
-	  console.log('statusCode:', response && response.statusCode); 	// Print the response status code if a response was received
-	  console.log('body:', body); 	  								
-	  trbmbContent= body;
-	})
-}
-
 
 
